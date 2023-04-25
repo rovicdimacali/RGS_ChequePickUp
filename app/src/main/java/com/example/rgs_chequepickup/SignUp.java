37a4c47a1e4470ecd63588988acc36aa80bb9c61
Darren Tiqui <tiqui152001@gmail.com>
2023-04-25 21:14:58 +0800

    Congratulations.java ForgotPassword.java activity_congratulations.xml activity_forgot_password.xml

--- app/src/main/java/com/example/rgs_chequepickup/SignUp.java
+++ app/src/main/java/com/example/rgs_chequepickup/SignUp.java
@@ -2,13 +2,36 @@ package com.example.rgs_chequepickup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SignUp extends AppCompatActivity {
-
+    TextView back_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        back_button = (TextView) findViewById(R.id.back_button);
+
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/fontawesome-webfont.ttf");
+
+        back_button.setTypeface(font);
+        back_button.setText("\uf060");
+
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openStartAct();
+            }
+        });
+    }
+    public void openStartAct() {
+        Intent intent = new Intent(this, StartActivity.class);
+        startActivity(intent);
     }
 }
