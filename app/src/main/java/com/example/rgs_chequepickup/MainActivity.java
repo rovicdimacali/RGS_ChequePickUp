83d7cb2a08736d735a4d7eafa83f110f9294708e
Rovic <rovic_luna19@yahoo.com>
2023-04-25 17:56:18 +0800

    FontAwesome Asset Main Activity Import Fontawesome Activity main xml Home Fragment

--- app/src/main/java/com/example/rgs_chequepickup/MainActivity.java
+++ app/src/main/java/com/example/rgs_chequepickup/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.rgs_chequepickup;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView home,  profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        home = (TextView) findViewById(R.id.icon_home);
+        profile = (TextView) findViewById(R.id.icon_profile);
+
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/fontawesome-webfont.ttf");
+
+        home.setTypeface(font);
+        profile.setTypeface(font);
+
+        home.setText("\uf015");
+        profile.setText("\uf007");
 
     }
 }
