ca945325c7043ba9baa0fd423a96f35c35d70735
Darren Tiqui <tiqui152001@gmail.com>
2023-04-25 17:54:38 +0800

    Activity Login xml Activity Sign Up xml Start Activity Class Sign Up Class

--- app/src/main/java/com/example/rgs_chequepickup/StartActivity.java
+++ app/src/main/java/com/example/rgs_chequepickup/StartActivity.java
@@ -2,13 +2,42 @@ package com.example.rgs_chequepickup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
-
+    private Button start_button;
+    private Button login_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        start_button = (Button) findViewById(R.id.start_button);
+        start_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignUp();
+            }
+        });
+
+        login_button = (Button) findViewById(R.id.login_button);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLogin();
+            }
+        });
+    }
+    public void openSignUp() {
+        Intent intent = new Intent(this, SignUp.class);
+        startActivity(intent);
+    }
+
+    public void openLogin(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
