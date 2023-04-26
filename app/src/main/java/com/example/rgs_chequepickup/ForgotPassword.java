271048227d5897f82961cf82c0c541b6f83b9a01
Darren Tiqui <tiqui152001@gmail.com>
2023-04-26 10:08:40 +0800

    Edited: ForgotPassword.java SignUp.java activity_sign_up.xml

--- app/src/main/java/com/example/rgs_chequepickup/ForgotPassword.java
+++ app/src/main/java/com/example/rgs_chequepickup/ForgotPassword.java
@@ -7,16 +7,23 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ForgotPassword extends AppCompatActivity {
+
+    private Button continue_button;
+
     TextView back_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
 
         back_button = (TextView) findViewById(R.id.back_button);
+
+        continue_button = (Button) findViewById(R.id.continue_button);
 
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/fontawesome-webfont.ttf");
 
@@ -26,12 +33,24 @@ public class ForgotPassword extends AppCompatActivity {
         back_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 openStartAct();
+            }
+        });
+
+        continue_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openOtp();
             }
         });
     }
     public void openStartAct() {
         Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+    public void openOtp(){
+        Intent intent = new Intent (this, OneTimePass.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/rgs_chequepickup/SignUp.java
+++ app/src/main/java/com/example/rgs_chequepickup/SignUp.java
@@ -10,6 +10,8 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class SignUp extends AppCompatActivity {
+
+    private Button login_button;
     TextView signing;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,8 @@ public class SignUp extends AppCompatActivity {
 
         signing = (TextView) findViewById(R.id.signing);
 
+        login_button = (Button) findViewById(R.id.login_button);
+
         signing.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,9 +29,21 @@ public class SignUp extends AppCompatActivity {
             }
         });
 
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCongrats();
+            }
+        });
+
     }
     public void openLogin(){
         Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void openCongrats(){
+        Intent intent = new Intent(this, Congratulations.class);
         startActivity(intent);
     }
 }
