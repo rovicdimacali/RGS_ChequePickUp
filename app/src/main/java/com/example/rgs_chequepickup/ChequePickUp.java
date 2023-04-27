d3ac68ff72a6bdb7c050c360bd7b089a93324b4f
Kphi <kurttimothy.paguio.cics@ust.edu.ph>
2023-04-27 21:08:20 +0800

    ChequePickUp.java

--- app/src/main/java/com/example/rgs_chequepickup/ChequePickUp.java
+++ app/src/main/java/com/example/rgs_chequepickup/ChequePickUp.java
@@ -3,13 +3,20 @@ package com.example.rgs_chequepickup;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ChequePickUp extends AppCompatActivity {
 
-    TextView icon_name, icon_location, icon_number;
+    TextView icon_name, icon_location, icon_number, address;
+    Button go_button, arrived_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,9 +24,15 @@ public class ChequePickUp extends AppCompatActivity {
         Fragment fragment = new MapFragment();
         getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, fragment).commit();
 
+        //TEXTVIEWS
         icon_name = (TextView) findViewById(R.id.icon_name);
         icon_location = (TextView) findViewById(R.id.icon_location);
         icon_number = (TextView) findViewById(R.id.icon_number);
+        address = (TextView) findViewById(R.id.address);//Address
+
+        //BUTTONS
+        go_button = (Button) findViewById(R.id.go_button);
+        arrived_button = (Button) findViewById(R.id.arrived_button);
 
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/fontawesome-webfont.ttf");
 
@@ -30,5 +43,36 @@ public class ChequePickUp extends AppCompatActivity {
         icon_name.setText("\uf007");
         icon_location.setText("\uf015");
         icon_number.setText("\uf2a0");
+
+        //LAUNCH GMAPS BASED ON ADDRESS
+        go_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String add = address.getText().toString();
+                if(add.equals("")){
+                    Toast.makeText(getApplicationContext(),"No location found", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    DisplayMap(add);
+                }
+                //Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("google.navigation:q="));
+            }
+        });
+    }
+
+    private void DisplayMap(String address){
+        try{
+            Uri uri = Uri.parse("google.navigation:q=" + address);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            intent.setPackage("com.google.android.apps.maps");
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
+        catch(ActivityNotFoundException e){
+            Uri uri = Uri.parse("https://play.google.com/store/apps/details?id=com.google.android.apps.maps");
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/rgs_chequepickup/SplashActivity.java
+++ app/src/main/java/com/example/rgs_chequepickup/SplashActivity.java
@@ -16,7 +16,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, CaptureCheque.class);
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
