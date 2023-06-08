6d7caa57f8021359ec40bb00e9e2acce6f040ac6
Rovic <rovic_luna19@yahoo.com>
2023-06-08 22:18:00 +0800

    asd

--- app/src/main/java/com/example/rgs_chequepickup/SplashActivity.java
+++ app/src/main/java/com/example/rgs_chequepickup/SplashActivity.java
@@ -1,25 +1,82 @@
 package com.example.rgs_chequepickup;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
+import android.widget.Toast;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static final int PERMISSION_REQUEST_CODE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashActivity.this, StartActivity.class);
-                startActivity(intent);
+        if (!isGPSEnabled()) {
+            // Show a dialog to the user to enable GPS
+            showEnableGPSDialog();
+        }else{
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(SplashActivity.this, StartActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, 3000);
+        }
+    }
+
+    private boolean isGPSEnabled() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+    private void showEnableGPSDialog() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("GPS is disabled. Do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Enable GPS",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // Open GPS settings activity
+                                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                startActivityForResult(intent, PERMISSION_REQUEST_CODE);
+                            }
+                        });
+        alertDialogBuilder.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                        finish();
+                    }
+                });
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            // Check if the user enabled GPS or not
+            if (isGPSEnabled()) {
+                // GPS is enabled, proceed with your app logic
+                // For example, start the main activity
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            } else {
+                // GPS is still not enabled, show a toast message
+                Toast.makeText(this, "GPS is still disabled. Exiting...", Toast.LENGTH_SHORT).show();
                 finish();
             }
-        }, 3000);
+        }
     }
 }
